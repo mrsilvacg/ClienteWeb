68394ac6be32997b813afe75bf83753b0545e9f8
mrsilvacg <mrsilva_cg@yahoo.cm.br>
2016-03-31 00:11:49 -0400

    Atualização 31/03
    Atualização checkbox

--- trabalho/script.js
+++ trabalho/script.js
@@ -43,123 +43,12 @@ document.cadastro.email.focus();
 return false;
 }
 else
-if(document.cadastro.newsSim.checked == false && document.cadastro.newsNao.checked == false){
+if(document.cadastro.sim.checked == false && document.cadastro.nao.checked == false){
 	alert("Escolha Sim ou Não");
 	document.cadastro.newsSim.focus();		
 		return false;
 }
 
-else
-return true;
-
-};
-
-function validaMsg(){
-
-if(document.cadastro.newsSim.checked == true && document.cadastro.newsNao.checked == false){
-	alert("A qualquer momento voce pode cancelar o recebimento dos e-mails de promoção\
-	enviando um e-mail com o assunto STOP MAIL para o endereço contato@estacio.br");
-
-	
-return false;
-
-}	
-	
-	
-};
-	
-function validaValores(){
-	var vnome = document.cadastro.nome.value;
-	var novoCpf = document.cadastro.cpf.value;
-	var vendereco = document.cadastro.endereco.value;
-	var vcidade = document.cadastro.cidade.value;
-	var vsexo = document.cadastro.sexo.value;
-	var vtelefone = document.cadastro.telefone.value;
-	var vemail = document.cadastro.email.value;
-	var vsenha = document.cadastro.senha.value;
-	var vpromocao;
-	if(document.cadastro.newsSim.checked == true){
-		var vpromocao = "SIM";
-	}else{
-		var vpromocao = "Não";
-	};
-	
-	
-	var vobservacao = document.cadastro.observacao.value;
-	
-	alert("CPF: " + novoCpf + "\nNOME: " + vnome + "\nENDEREÇO: " + vendereco + "\nCIDADE: " + vcidade + "\nSEXO: " + vsexo 
-			+ "\nTELEFONE: " + vtelefone +  "\nE-MAIL: " + vemail +"\nSENHA: " + vsenha 
-				+  "\nPROMOÇÂO: " + vpromocao + "\nOBSERVAÇÂO: " + vobservacao);	
-	
-	
-	
-};
-
-function validaNome(e){
-	
-	 tecla = event.keyCode;
-		
-		if (tecla >= 33 && tecla <= 64 
-			|| tecla >= 91 && tecla <= 93 
-				|| tecla >= 123 && tecla <= 159 
-					|| tecla >= 162 && tecla <= 191 ){ 
-			return false;
-			
-		}else{
-		   return true;
-		}
-};
-
-function validaMaiuscula(z){
-        v = z.value.toUpperCase();
-        z.value = v;
-    };
-
-function validaSenha(){
-	if(document.cadastro.senha.value=="" 
-		|| document.cadastro.senha.value.length < 7
-			|| document.cadastro.senha.value.indexOf(' ','0')!= -1)
-			{
-				alert("Digite uma senha válida!");
-				document.cadastro.senha.focus();
-			return false;
-}
-	
-
-};
-
-function mascaraTelefone(mascara, documento){
-  var i = documento.value.length;
-  var saida = mascara.substring(0,1);
-  var texto = mascara.substring(i)
-  
-  if (texto.substring(0,1) != saida){
-            documento.value += texto.substring(0,1);
-  }
-  
-};
-
-function validaEmail(){
-if( document.forms[0].email.value=="" 
-   || document.forms[0].email.value.indexOf('@')==-1 
-     || document.forms[0].email.value.indexOf('.')==-1 )
-	{
-		alert( "Por favor, informe um E-MAIL válido!" );
-		document.cadastro.email.focus();
-		return false;
-	}
-};
-
-function validaObservacao(){
-
-   var TextMsg =  cadastro.observacao.value;
-   if (TextMsg.length>200)
-   {
-       alert("Permitido 200 caracteres!!!!");
-	   document.cadastro.observacao.focus();
-   return false;
-    }
-   
 };
 
 function validarCPF( cpf ){
@@ -249,6 +138,132 @@ function cpf_mask(v){
 	v=v.replace(/(\d{3})(\d)/,"$1.$2")    //Coloca ponto entre o setimo e o oitava dígitos
 	v=v.replace(/(\d{3})(\d)/,"$1-$2")   //Coloca ponto entre o decimoprimeiro e o decimosegundo dígitos
 	return v
+};
+
+//valida nome 
+function validaNome(e){
+	
+	 tecla = event.keyCode;
+		
+		if (tecla >= 33 && tecla <= 64 
+			|| tecla >= 91 && tecla <= 93 
+				|| tecla >= 123 && tecla <= 159 
+					|| tecla >= 162 && tecla <= 191 ){ 
+			return false;
+			
+		}else{
+		   return true;
+		}
+};
+
+//valida letras maiusculas
+function validaMaiuscula(z){
+        v = z.value.toUpperCase();
+        z.value = v;
+    };
+
+//valida formato da mascara do telefone	
+function mascaraTelefone(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+};
+
+//valida formato e-mail
+function validaEmail(){
+if( document.forms[0].email.value=="" 
+   || document.forms[0].email.value.indexOf('@')==-1 
+     || document.forms[0].email.value.indexOf('.')==-1 )
+	{
+		alert( "Por favor, informe um E-MAIL válido!" );
+		document.cadastro.email.focus();
+		return false;
+	}
+};
+
+//valida senha 
+function validaSenha(){
+	if(document.cadastro.senha.value=="" 
+		|| document.cadastro.senha.value.length < 7
+			|| document.cadastro.senha.value.indexOf(' ','0')!= -1)
+			{
+				alert("Digite uma senha válida!");
+				document.cadastro.senha.focus();
+			return false;
+}
+	
+
+};
+
+
+//validação checkbox com a mensagem
+function validaMsg(){
+		
+	if(document.cadastro.newsSim.checked  && document.cadastro.newsNao.checked || 
+		document.cadastro.newsSim.checked == false  && document.cadastro.newsNao.checked == false){
+	alert("Campo Obrigatório - Escolha Sim ou Não");
+			
+	return false;
+	}
+	
+	if(document.cadastro.newsSim.checked == true && document.cadastro.newsNao.checked == false){
+		
+	document.getElementById("msgpromocao").innerHTML = "A qualquer momento voce pode cancelar o recebimento dos e-mails de promoção\
+	enviando um e-mail com o assunto STOP MAIL para o endereço contato@estacio.br";
+	}
+	else{
+		document.getElementById("msgpromocao").innerHTML = "";
+	}
+	return false;
+
+		
+};
+
+//Valida campo observacao
+function validaObservacao(){
+
+   var TextMsg =  cadastro.observacao.value;
+   if (TextMsg.length>200)
+   {
+       alert("Permitido 200 caracteres!!!!");
+	   document.cadastro.observacao.focus();
+   return false;
+    }
+   
+};
+
+
+//valida valores para exibir no alerta	
+function validaValores(){
+	var vnome = document.cadastro.nome.value;
+	var novoCpf = document.cadastro.cpf.value;
+	var vendereco = document.cadastro.endereco.value;
+	var vcidade = document.cadastro.cidade.value;
+	var vsexo = document.cadastro.sexo.value;
+	var vtelefone = document.cadastro.telefone.value;
+	var vemail = document.cadastro.email.value;
+	var vsenha = document.cadastro.senha.value;
+	var vpromocao;
+	if(document.cadastro.newsSim.checked == true){
+		var vpromocao = "SIM";
+	}else{
+		var vpromocao = "Não";
+	};
+	
+	
+	var vobservacao = document.cadastro.observacao.value;
+	
+	alert("CPF: " + novoCpf + "\nNOME: " + vnome + "\nENDEREÇO: " + vendereco + "\nCIDADE: " + vcidade + "\nSEXO: " + vsexo 
+			+ "\nTELEFONE: " + vtelefone +  "\nE-MAIL: " + vemail +"\nSENHA: " + vsenha 
+				+  "\nPROMOÇÂO: " + vpromocao + "\nOBSERVAÇÂO: " + vobservacao);	
+	
+	
+	
 };
 
 <!-- Fim do JavaScript que validará os campos obrigatórios! -->
